b30992743e9399dc31bba74d315dbfb74da2a61f
Nico Pampe <nicopampe@gmail.com>
2017-04-28 13:10:15 -0600

    Factorio dataset is added

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 
 
-drop table if exists lotr_words cascade;
+DROP TABLE IF EXISTS lotr_words cascade;
 CREATE TABLE lotr_words(
 	id serial, 
 	film TEXT, 
@@ -11,3 +11,10 @@ CREATE TABLE lotr_words(
 -- copies from a file in the same directory. Wierd syntax for the Flowers db.
 \copy lotr_words(film, chapter, character, race, words) FROM 'lotr_wordsSpoken.tsv';
 
+DROP TABLE IF EXISTS factorio_recipe cascade;
+CREATE TABLE factorio_recipe(
+	id serial, 
+	recipe TEXT, 
+	resources TEXT, 
+	amount TEXT); 
+\copy factorio_recipe(recipe, resources, amount) FROM 'factorio_recipe.csv' WITH (DELIMITER ',');
