5869a9c63c0cd03d8b6f5143deee8a3c09fdff48
Nico Pampe <nicopampe@gmail.com>
2017-05-02 08:04:38 -0600

    Code is broken. Pushing what I have. CSV file seems to be different for Cpws copy command

--- schema.sql
+++ schema.sql
@@ -17,4 +17,36 @@ CREATE TABLE factorio_recipe(
 	recipe TEXT, 
 	resources TEXT, 
 	amount TEXT); 
+-- copies from a file in the same directory. Wierd syntax for the Flowers db.
 \copy factorio_recipe(recipe, resources, amount) FROM 'factorio_recipe.csv' WITH (DELIMITER ',');
+
+DROP TABLE IF EXISTS war_of_five_kings cascade;
+CREATE TABLE war_of_five_kings(
+	id serial, 
+	name TEXT,
+	year TEXT,
+	battle_number TEXT,
+	attacker_king TEXT,
+	defender_king TEXT,
+	attacker_1 TEXT,
+	attacker_2 TEXT,
+	attacker_3 TEXT,
+	attacker_4 TEXT,
+	defender_1 TEXT,
+	defender_2 TEXT,
+	defender_3 TEXT,
+	defender_4 TEXT,
+	attacker_outcome TEXT,
+	battle_type TEXT,
+	major_death TEXT,
+	major_capture TEXT,
+	attacker_size TEXT,
+	defender_size TEXT,
+	attacker_commander TEXT,
+	defender_commander TEXT,
+	summer TEXT,
+	location TEXT,
+	region TEXT,
+	note TEXT);
+\copy war_of_five_kings(name,year,battle_number,attacker_king,defender_king,attacker_1,attacker_2,attacker_3,attacker_4,defender_1,defender_2,defender_3,defender_4,attacker_outcome,battle_type,major_death,major_capture,attacker_size,defender_size,attacker_commander,defender_commander,summer,location,region,note) FROM 'war_of_the_five_kings_dataset.csv' WITH (DELIMITER ',');
+
